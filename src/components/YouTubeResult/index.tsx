4b78f47c9ce8889a1b9625b11171e6a40fed3603
Henri <henri@ember-music.app>
2026-06-21 02:23:44 -0300

    chore: increase row gap to gap-y-12

--- src/components/YouTubeResult/index.tsx
+++ src/components/YouTubeResult/index.tsx
@@ -26,7 +26,7 @@ export function YouTubeResult({ items, query }: YouTubeResultProps) {
   return (
     <>
       <section>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-12">
           {items.map((item) => (
             <button
               key={item.videoId}
